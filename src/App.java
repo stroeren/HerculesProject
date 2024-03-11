d5aa8cd662152e3ba52f7299ff918d16fc58a234
stroeren <stroeren@gmail.com>
2024-03-11 19:31:28 -0400

    Game base

--- src/App.java
+++ src/App.java
@@ -1,5 +1,10 @@
 import com.almasb.fxgl.app.GameApplication;
 import com.almasb.fxgl.app.GameSettings;
+import com.almasb.fxgl.dsl.FXGL;
+import com.almasb.fxgl.entity.Entity;
+import com.almasb.fxgl.input.UserAction;
+
+import javafx.scene.input.KeyCode; 
 
 public class App extends GameApplication {
 
@@ -13,5 +18,31 @@ public class App extends GameApplication {
 
     public static void main(String[] args) {
         launch(args);
+    }
+
+    @Override
+    protected void initGame() {
+        FXGL.entityBuilder()
+            .at(300, 300) // Set the initial position of the player
+            .view("player.png") // Set the player's sprite or view
+            .buildAndAttach();
+    }
+    
+    @Override
+    protected void initInput() {
+        Entity player = FXGL.entityBuilder()
+            .at(0, 0)
+            .buildAndAttach();
+
+        FXGL.getInput().addAction(new UserAction("Move Right") {
+            @Override
+            protected void onAction() {
+                player.translateX(5); // Move the player 5 units to the right
+            }
+        }, KeyCode.D); // Bind the action to the D key
+    }
+    @Override
+    protected void onUpdate(double tpf) {
+        // Game logic here, executed every frame
     }
 }
