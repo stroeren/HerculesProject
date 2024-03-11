d64369b2efd0f9773a1f1fb97d433467f74b16e1
stroeren <stroeren@gmail.com>
2024-03-11 19:20:44 -0400

    First start of game

--- src/App.java
+++ src/App.java
@@ -1,5 +1,17 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+import com.almasb.fxgl.app.GameApplication;
+import com.almasb.fxgl.app.GameSettings;
+
+public class App extends GameApplication {
+
+    @Override
+    protected void initSettings(GameSettings settings) {
+        settings.setTitle("My Game");
+        settings.setVersion("0.1");
+        settings.setWidth(800); // Set the width of the window
+        settings.setHeight(600); // Set the height of the window
+    }
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
