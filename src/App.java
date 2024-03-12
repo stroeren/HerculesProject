e38cca6726b37c22d75d13147ea605b756df115a
stroeren <stroeren@gmail.com>
2024-03-11 21:50:21 -0400

    Increased Window size

--- src/App.java
+++ src/App.java
@@ -12,8 +12,8 @@ public class App extends GameApplication {
     protected void initSettings(GameSettings settings) {
         settings.setTitle("My Game");
         settings.setVersion("0.1");
-        settings.setWidth(800); // Set the width of the window
-        settings.setHeight(600); // Set the height of the window
+        settings.setWidth(1000); // Set the width of the window
+        settings.setHeight(800); // Set the height of the window
     }
 
     public static void main(String[] args) {
