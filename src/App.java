4e63a00b52b2483116b3ca92596556efa847c46f
stroeren <stroeren@gmail.com>
2024-03-28 14:07:03 -0400

    Added FXGL Imports

--- src/App.java
+++ src/App.java
@@ -1,10 +1,24 @@
-import com.almasb.fxgl.app.GameApplication;
-import com.almasb.fxgl.app.GameSettings;
+
 import com.almasb.fxgl.dsl.FXGL;
 import com.almasb.fxgl.entity.Entity;
 import com.almasb.fxgl.input.UserAction;
-
-import javafx.scene.input.KeyCode; 
+import static com.almasb.fxgl.dsl.FXGL.getAppHeight;
+import static com.almasb.fxgl.dsl.FXGL.getAppWidth;
+import static com.almasb.fxgl.dsl.FXGL.getGameController;
+import static com.almasb.fxgl.dsl.FXGL.getGameWorld;
+import static com.almasb.fxgl.dsl.FXGL.onBtnDown;
+import static com.almasb.fxgl.dsl.FXGL.onCollisionBegin;
+import static com.almasb.fxgl.dsl.FXGL.onKey;
+import static com.almasb.fxgl.dsl.FXGL.run;
+import static com.almasb.fxgl.dsl.FXGL.showMessage;
+import static com.almasb.fxgl.dsl.FXGL.spawn;
+import com.almasb.fxgl.app.GameApplication;
+import com.almasb.fxgl.app.GameSettings;
+import com.almasb.fxgl.entity.Entity;
+import com.almasb.fxgl.entity.EntityFactory;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.MouseButton;
+import javafx.util.Duration; 
 
 public class App extends GameApplication {
 
